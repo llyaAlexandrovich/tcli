815356908ca54a0cc0ec9c67abef8315db38e924
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-01-15 15:59:32 +0200

    Done with internal filesystem api for now. 	modified:   src/fs/fs.cpp 	modified:   src/fs/fs.hpp 	modified:   thirdparty/logging/@spdlog

--- src/fs/fs.cpp
+++ src/fs/fs.cpp
@@ -2,3 +2,47 @@
 
 
 
+
+
+
+
+namespace fs
+{
+    std::size_t GetFileSize(std::string_view FileName) noexcept
+    {
+        std::error_code ec;
+        return std::filesystem::file_size(FileName, ec);
+    }
+
+
+
+    std::string ReadFile(std::string_view FileName) noexcept
+    {
+        std::ifstream FileStream(FileName.data());
+        std::string output;
+
+        if(!FileStream.is_open())
+        {
+            return output;
+        }
+
+        while(std::getline(FileStream, output));
+
+        FileStream.close();
+
+        return output;
+    }
+
+
+
+    std::string FindFile(std::string_view FileName) noexcept
+    {
+        for(const auto& entry: std::filesystem::recursive_directory_iterator(std::filesystem::current_path()))
+        {
+            if(entry.is_regular_file() && entry.path().filename() == FileName)
+            {
+                return entry.path().string();
+            }
+        }
+    }
+}
--- src/fs/fs.hpp
+++ src/fs/fs.hpp
@@ -3,12 +3,64 @@
 
 
 #include <iostream>
+#include <string>
+#include <string_view>
 #include <filesystem>
 #include <fstream>
+#include <exception>
 
 
 #include "spdlog/spdlog.h"
 
+/**
+ * Internal filesystem namespace.
+ * 
+ * @author Ilya Alexandrovich
+ * 
+ * @since 1.0.0
+ */
+namespace fs
+{
+    /**
+     * Get size of the file.
+     * 
+     * @author Ilya Alexandrovich
+     * 
+     * @param FileName  read-only name of the file
+     * 
+     * @return return either size of the file or 0 in error
+     * 
+     * @since 1.0.0
+     */
+    std::size_t GetFileSize(std::string_view FileName) noexcept;
 
 
 
+    /**
+     * Read entire file and return file content.
+     * 
+     * @author Ilya Alexandrovich
+     * 
+     * @param FileName  read-only name of the file
+     * 
+     * @return return either file content or std::string.empty() == true
+     * 
+     * @since 1.0.0
+    */
+    std::string ReadFile(std::string_view FileName) noexcept;
+
+
+
+    /**
+     * Search for a file in the current and all included directories.
+     * 
+     * @author Ilya Alexandrovich
+     * 
+     * @param FileName read-only name of the file
+     * 
+     * @return return either file content or std::string.empty() == true
+     * 
+     * @since 1.0.0
+     */
+    std::string FindFile(std::string_view FileName) noexcept;
+}
