e5a0bc34a9bf6756213be3e7b183aed7877b7e53
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-01-15 13:18:29 +0200

    modified:   .gitignore

--- scripts/configure.py
+++ scripts/configure.py
@@ -15,30 +15,29 @@ version = "1.0.0"
 
 if __name__ == "__main__":
     # Normalize working directory to work from.
-    path = os.getcwd()
+    CurrentPath = os.getcwd()
 
-    if "scripts" in path:
+    if "scripts" in CurrentPath:
         os.chdir("..")
-        path = os.getcwd()
+        CurrentPath = os.getcwd()
 
 
     # Create .env file with initial values from .env.global .
     EnvGlobal = open(".env.global")
-    with open(".env", 'w', encoding="utf-8") as env:
-        env.write(EnvGlobal.read())
-    
+    with open(".env", 'w', encoding="utf-8") as f:
+        f.write(EnvGlobal.read())
     EnvGlobal.close()
 
 
     # Add some machine dependent variables in .env .
-    with open(".env", 'a', encoding="utf-8") as env:
-        env.write(f"\nOS={os.name}\n")
+    with open(".env", 'a', encoding="utf-8") as f:
+        f.write(f"\nOS={os.name}\n")
 
 
     # Get environment variables.
     env = dotenv_values(".env")
-    InternalEnv = os.environ["PATH"]
-    InternalEnvDict = InternalEnv.split(';')
+    PathEnv = os.environ["PATH"]
+    PathEnvDict = PathEnv.split(';')
     
 
     # Initialize logging.
@@ -50,7 +49,7 @@ if __name__ == "__main__":
                             logging.StreamHandler(sys.stdout),
                             logging.FileHandler("temp/configure.log", mode='a')
                         ])
-    
+
 
     logging.info("Logging initialized successfully")
     logging.info("Logging file - temp/configure.log")
@@ -60,35 +59,36 @@ if __name__ == "__main__":
 
 
     # Checking if current version is the newest.
-    #try:
-    #    response = requests.get("https://raw.githubusercontent.com/llyaAlexandrovich/tcli/refs/heads/main/.env.global", timeout=3)
-    #    if response.status_code == 200:
-    #        logging.info("Get data from remote repositoy - https://github.com/llyaAlexandrovich/tcli")
-    #        logging.info("Creating temporary file in - temp/")
-    #
-    #        with open("temp/.env.temp", 'w', encoding="utf-8") as TempEnv:
-    #            TempEnv.write(response.text)
-    #
-    #        logging.info("Temporary file name - .env.temp")
-    #
-    #        RemoteEnv = dotenv_values("temp/.env.temp")
-    #
-    #        if RemoteEnv["VERSION"] != env["VERSION"]:
-    #            logging.info("Newly version available. Consider updating")
-    #        else:
-    #            logging.info("Current version considered newest")
-    #    else:
-    #        logging.warning("Unable to fetch data from official repository. Recommend to update manually")
-    #except:
-    #    logging.error("HTTP error occurred. Unable to fetch from remote repository")
-    #    logging.warning("Unable to determine whether the newest version is used")
-    #    logging.warning("Version fetching skipped")
+    try:
+        response = requests.get("https://raw.githubusercontent.com/llyaAlexandrovich/tcli/refs/heads/main/.env.global", timeout=3)
+        if response.status_code == 200:
+            logging.info("Get data from remote repositoy - https://github.com/llyaAlexandrovich/tcli")
+            logging.info("Creating temporary file in - temp/")
+    
+            with open("temp/.env.temp", 'w', encoding="utf-8") as TempEnv:
+                TempEnv.write(response.text)
+    
+            logging.info("Temporary file name - .env.temp")
+    
+            RemoteEnv = dotenv_values("temp/.env.temp")
+    
+            if RemoteEnv["VERSION"] != env["VERSION"]:
+                logging.info("Newly version available. Consider updating")
+            else:
+                logging.info("Current version considered newest")
+        else:
+            logging.warning("Unable to fetch data from official repository. Recommend to update manually")
+    except:
+        logging.error("HTTP error occurred. Unable to fetch from remote repository")
+        logging.warning("Unable to determine whether the newest version is used")
+        logging.warning("Version fetching skipped")
     
 
+
     # Check internal required dependencies.
-    if "mingw" and "cmake" not in InternalEnv.lower():
+    if "mingw" and "cmake" not in PathEnv.lower():
         logging.critical("Lack of dependencies")
-        logging.critical("GCC-14, G++-14 and CMake-4.2 required to build this project")
+        logging.critical("GCC-14, G++-14, git and CMake-4.2 required to build this project. Doxygen and mkdocs might also be used but not strictly required")
         logging.info("If required dependencies are installed try specify them using .env file and restart the program")
         exit()
 
@@ -96,32 +96,26 @@ if __name__ == "__main__":
     # Add additional requried data to .env .
     logging.info("Add dependencies data to the .env")
     if env["OS"] == "nt":
-        for arg in InternalEnvDict:
+        for arg in PathEnvDict:
             if "mingw" in arg:
                 logging.info("Add gcc absolute path")
                 logging.info("Add gxx absolute path")
-                with open(".env", 'a', encoding="utf-8") as Env:
-                    Env.write(f"GCC={arg}\\gcc.exe\n")
-                    Env.write(f"GXX={arg}\\g++.exe\n")
+                with open(".env", 'a', encoding="utf-8") as f:
+                    f.write(f"GCC={arg}\\gcc.exe\n")
+                    f.write(f"GXX={arg}\\g++.exe\n")
             if "cmake" in arg.lower():
                     logging.info("Add cmake absolute path")
-                    with open(".env", 'a', encoding="utf-8") as Env:
-                        Env.write(f"CMAKE={arg}\\cmake.exe\n")
-    elif env["OS"] == "posix":
-        pass
+                    with open(".env", 'a', encoding="utf-8") as f:
+                        f.write(f"CMAKE={arg}\\cmake.exe\n")
+            if "git" in arg.lower():
+                logging.info("Add git absolute path")
+                with open(".env", 'a', encoding="utf-8") as f:
+                    f.write(f"GIT={arg}\\git.exe\n")
+    # TODO: Linux part is still not done yet.
+    #elif env["OS"] == "posix":
+    #    pass
 
 
     # Resolving dependencies.
     logging.info("Start resolving dependencies")
-
-    # Run program 3 times before exitins with error critical.
-    counter = 0
-
-    while counter != 3:
-        counter += 1
-        InitDepManager(version)
-
-
-    logging.critical("Program ran out of attempts - 3/3")
-    logging.critical("Dependencies not resolved")
-
+    InitDepManager(version)
--- scripts/depmanager.py
+++ scripts/depmanager.py
@@ -1,45 +1,204 @@
 import os
-import sys
+import shutil
 import glob
-import pathlib
 import logging
 import time
 import threading
 import requests
 
 
+# TODO: COMPILED flag in blob files is useless for now since we don't
+# TODO: really resolving any dependencies.
 
 
 
 
-def Ping(response: list[requests.Response]):
+def Ping(response: list):
     try:
         logging.info("Establishing internet connection")
         logging.info("Checking github.com availability")
-        response[0] = requests.get(url="https://github.com/llyaAlexandrovich/tcli", timeout=20)
+        response[0] = requests.get(url="https://github.com/llyaAlexandrovich/tcli", timeout=20).status_code
     except:
         logging.error("HTTP error occurred. Unable to connect to the remote server")
 
 
 
-def RunBlob(BlobInfo: dict,
-            BlobName: str):
-    logging.info(f"Trying to run blob - {BlobInfo[BlobName]["NAME"]}")
-    logging.info("Sheduling tasks")
+def RunBlob(FileName: str,
+            BlobInfo: list):
+    logging.info(f"Trying to run blob - {FileName}")
+    logging.info(f"Change working directory to - {os.path.dirname(FileName)}")
+    os.chdir(os.path.dirname(FileName))
+
+
+    logging.info("Clonning external repository")
+    os.system(f"git clone {BlobInfo[1]["LINK"]} --depth 1")
+
+    # TODO: We don't check whether these values is already in config or not so they will be
+    # TODO: written every time. NOTE: Not REwritten. Just written.
+    logging.info(f"BLOB_SOURCE_DIRECTORY={os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}")
+    logging.info(f"BLOB_BIN_DIRECTORY={os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\\build")
+    with open(f"{BlobInfo[1]["NAME"]}.conf", 'a', encoding="utf-8") as f:
+        f.write(f"BLOB_SOURCE_DIRECTORY={os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\nBLOB_BIN_DIRECTORY={os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\\build")
+
+
+    logging.info("Start building process")
+
+    logging.info(f"Change working directory to - {os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}")
+    os.chdir(f"{os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}")
+
+
+    logging.info("Creating required directories - build")
+    try:
+        os.mkdir("build")
+    except:
+        pass
+
+
+    logging.info(f"Change working directory to - {os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\\build")
+    os.chdir(f"{os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\\build")
+
+
+    logging.info("Executing commands")
+
+    logging.info("cmake -G \"MinGW Makefiles\" ..")
+    os.system("cmake -G \"MinGW Makefiles\" ..")
+
+    logging.info("cmake --build . --parallel 8")
+    os.system("cmake --build . --parallel 8") # TODO: Number of cores may vary from system to system. This number needs to be reconsidered and automatically changed.
+
+
+    logging.info("Copying files")
+    logging.info("Copying bins to - thirdparty/lib")
+    bins = str(BlobInfo[1]["BIN"]).split(' ')
+    for bin in bins: 
+        shutil.copy(bin, "..\\..\\..\\lib\\")
+
+
+    logging.info(f"Change working directory to - {os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\\include\\{BlobInfo[1]["NAME"]}")
+    os.chdir(f"{os.path.dirname(FileName)}\\{BlobInfo[1]["NAME"]}\\include")
+    logging.info(f"Copying includes to - thirdparty\\include{BlobInfo[1]["NAME"]}")
+    shutil.copytree(f"{BlobInfo[1]["NAME"]}", f"..\\..\\..\\include\\{BlobInfo[1]["NAME"]}")
 
 
 
-def RunBlobs():
-    pass
+def RunBlobs(BlobsInfo: dict,
+            BlobFileNames: list):
+    logging.info("Create required directories - thirdparty/include thirdparty/lib")
+    try:
+        os.mkdir("thirdparty/include")
+        os.mkdir("thirdparty/lib")
+    except:
+        pass
+
+    # As long as internet connection is required we checking
+    # for it first.
+    logging.info("Checking internet connection")
+
+    response = [None]
+    threading.Thread(target=Ping, args=[response]).start()
+
+
+    # Counting server timeout. Drop task if server is not responding.
+    TimeStart = time.time()
+    while response[0] == None:
+        if (time.time() - TimeStart) >= 30:
+            logging.critical("Unable to connect to the remote server")
+            logging.error("Server timeout reached")
+            logging.info("Check your internet connection and try again")
+            logging.critical("Unable to satisfy dependencies")
+            return
+
+
+    logging.info("Internet connection established")
+    logging.info("Trying to run blobs")
+
+    # Running blobs one by one until all the dependencies resolved.
+    for FileName in BlobFileNames:
+        if BlobsInfo[FileName][0] == True:
+            RunBlob(FileName, BlobsInfo[FileName])
 
 
 
-def ResolveBlob():
-    pass
+def ResolveBlob(version: str,
+                BlobFileName: str):
+    # Initial log for blob.
+    logging.info(f"Start resolving blob - {BlobFileName}")
+
+
+    # Checking for blob config file.
+    logging.info("Checking for blob config")
+
+
+    # TODO: Sooner or later i will need to optimize it cause we cannot
+    # TODO: run this function again and again for every single absent version
+    # TODO: in blobs config.
+    CompilationRequired = True
+    # If blob configuration absent write the default one.
+    if not os.path.exists(f"{os.path.dirname(BlobFileName)}\\{os.path.basename(BlobFileName)[1:]}.conf"):
+        logging.info("Config not found")
+        logging.info(f"Creating config for blob - {os.path.basename(BlobFileName)[1:]}")
+        logging.info(f"Config directory - {os.path.dirname(BlobFileName)}")
+        logging.info(f"Config file name - {os.path.dirname(BlobFileName)}\\{os.path.basename(BlobFileName)[1:]}.conf")
+        with open(f"{os.path.dirname(BlobFileName)}\\{os.path.basename(BlobFileName)[1:]}.conf", 'w', encoding="utf-8") as f:
+            f.write(f"DEPMANAGER_VERSION={version}\rBLOB_FILE_NAME={BlobFileName}\rCOMPILED={False}\n")
+    # If blob is already configured(config file exists) check config's depmanager version
+    # with current depmanager version and if not equal create new config and delete the old one.
+    else:
+        logging.info("Config found")
+        logging.info("Checking existed config")
+        with open(f"{os.path.dirname(BlobFileName)}\\{os.path.basename(BlobFileName)[1:]}.conf", 'r', encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                StringContent = line.strip().split('=')
+                match StringContent[0]:
+                    case "DEPMANAGER_VERSION":
+                        if StringContent[1] != version:
+                            logging.warning(f"The versions differ in config - {os.path.dirname(BlobFileName)}\\{os.path.basename(BlobFileName)[1:]}.conf")
+                            logging.info("Force program to renew config for current version")
+                            logging.info("Removing existed config file")
+                            f.close()
+                            os.remove(f"{os.path.dirname(BlobFileName)}\\{os.path.basename(BlobFileName)[1:]}.conf")
+                            ResolveBlob(version, BlobFileName)
+                    case "COMPILED":
+                        if StringContent[1] != "True":
+                            logging.info(f"Compilatioin required for blob - {os.path.basename(BlobFileName)}")
+                        else:
+                            logging.info(f"Compilation is not required for blob - {os.path.basename(BlobFileName)}")
+                            CompilationRequired = False
+
+
+    if CompilationRequired:
+        logging.info(f"Resolving blob - {BlobFileName}")
+
+        # Resolving blob itself.
+        FileContent = {}
+        with open(BlobFileName, 'r', encoding="utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                StringContent = line.strip().split('=')
+                FileContent[StringContent[0]] = StringContent[1]
+
+        logging.info("Blob's info")
+        logging.info(f"Blob's name - {FileContent["NAME"]}")
+        logging.info(f"Blob's link - {FileContent["LINK"]}")
+        logging.info(f"Blob's dependencies - {FileContent["DEPS"]}")
+        logging.info(f"Blob's dependent - {FileContent["OTHERS_DEP"]}")
+        logging.info(f"Blob's generator - {FileContent["GEN"]}")
+        logging.info(f"Blob's compilation script - {FileContent["SCRIPT"]}")
+        logging.info(f"Blob's bins - {FileContent["BIN"]}")
+        logging.info(f"Blob's includes - {FileContent["INCLUDE"]}")
+
+        # Output value contains all data collected from blob.
+        output = [CompilationRequired, FileContent] # [CompilationRequired(True|False){FileContent(dict)}]
+
+        return output
+    return [False]
 
 
 
-def ResolveBlobs():
+def ResolveBlobs(version: str):
+    logging.info("Trying to resolve blobs")
+
     # This values is initial and required for blobs processing not blobs tasking.
     BlobFileNames = [] # Full path to every known blob file founded.
     BlobsInfo     = {} # [BlobFileNames[]][""] like dictionary.
@@ -52,15 +211,15 @@ def ResolveBlobs():
     for file in glob.glob("thirdparty\\**\\@*"):
         logging.info(f"Blob file found - {os.path.abspath(file)}")
         BlobFileNames.append(os.path.abspath(file))
+    
+    logging.info("Trying to resolve blobs")
 
-
-    logging.info("Checking for blobs config")
-
-    # Check for $(blobName).conf file in every directory where
-    # blob files were found.
+    # Run blobs.
     for FileName in BlobFileNames:
-        pass
+        BlobsInfo[FileName] = ResolveBlob(version, FileName)
 
+
+    return BlobsInfo, BlobFileNames
 
 
 
@@ -70,115 +229,11 @@ def InitDepManager(version: str):
     logging.info("Logger inherited successfully")
 
 
-    # Get abspath. NOTE: there's no reason to do so yet.
+    # Get abspath.
     CurrentDirectory = os.path.abspath('.')
     logging.info(f"Current directory - {CurrentDirectory}")
 
 
-    # TODO: THIS CODE DOWN THERE IS SIMPLY AWFULL. REWRITE IT. ---------
-
-
-    # Iterate through thirdparty directory and find all the
-    # blob(@filename) files.
-    logging.info("Searching for blobs")
-    BlobInfo  = {}
-    BlobTasks = {}
-    BlobFiles = []
-
-    for file in glob.glob("thirdparty\\**\\@*"):
-        logging.info(f"Blob file found - {os.path.abspath(file)}")
-        BlobFiles.append(os.path.abspath(file))
-
-
-    logging.info("Extracting info from blobs")
-
-
-    # Getting info from blobs.
-    for file in BlobFiles:
-        FileContent = {}
-        # Reading data from blobs.
-        with open(file, 'r', encoding="utf-8") as f:
-            lines = f.readlines()
-            for line in lines:
-                StringContent = line.strip().split('=')
-                FileContent[StringContent[0]] = StringContent[1]
-                
-        logging.info("Blob's info")
-        logging.info(f"Blob's name - {FileContent["NAME"]}")
-        logging.info(f"Blob's link - {FileContent["LINK"]}")
-        logging.info(f"Blob's dependencies - {FileContent["DEPS"]}")
-        logging.info(f"Blob's dependent - {FileContent["OTHERS_DEP"]}")
-        logging.info(f"Blob's generator - {FileContent["GEN"]}")
-        logging.info(f"Blob's compile script - {FileContent["SCRIPT"]}")
-
-
-        # Creating .conf file for every blob so we could skip
-        # any already processed steps. Configs are created only
-        # if there's none already.
-        if not os.path.exists(f"{os.path.dirname(file)}\\{FileContent["NAME"]}.conf"):
-            logging.info(f"Creating config for blob - {FileContent["NAME"]}")
-            logging.info(f"Config directory - {os.path.dirname(file)}")
-            logging.info(f"Config file name - {os.path.dirname(file)}\\{FileContent["NAME"]}.conf")
-            with open(f"{os.path.dirname(file)}\\{FileContent["NAME"]}.conf", 'w', encoding="utf-8") as f:
-                f.write(f"DEPMANAGER_VERSION={version}\rBLOB_FILE_NAME={file}\rCONFIGURED={False}\rCOMPILED={False}")
-        else:
-            with open(f"{os.path.dirname(file)}\\{FileContent["NAME"]}.conf", 'r', encoding="utf-8") as f:
-                lines = f.readlines()
-                for line in lines:
-                    StringContent = line.strip().split('=')
-                    match StringContent[0]:
-                        case "DEPMANAGER_VERSION":
-                            if StringContent[1] != version:
-                                logging.warning(f"Versions diverge")
-                                logging.info("Force program to renew configs for current version")
-                                logging.info("Removing existed config files")
-                                break
-                                
-                    break
-            
-            # Remove old config file 
-            os.remove(f"{os.path.dirname(file)}\\{FileContent["NAME"]}.conf")
-                                
-
-        
-
-        BlobInfo[file] = FileContent
-        BlobTasks[file] = False
-
-
-    # TODO: ----------------------------------------------------------------
-
-    
-    logging.info("Trying to run blobs")
-    
-    # Running blobs.
-    # Trying to find blobs with no dependencies and run them first.
-    while True:
-        for blob in BlobTasks:
-            if BlobTasks[blob] == False:
-                if BlobInfo[blob]["DEPS"] == "":
-                    # Checking internet connection.
-                    # Try ping required servers.
-                    response = [None]
-                    thread = threading.Thread(target=Ping, args=[response]).start()
-
-                    # Counting server timeout. Drop task if server not responding.
-                    TimeStart = time.time()
-                    while response[0] == None:
-                        if (time.time() - TimeStart) >= 30:
-                            logging.error("Server timeout reached")
-                            logging.critical("Unable to satisfy dependencies")
-                            return
-    
-
-                    # Server is up and running.
-                    logging.info("Server connection established")
-                    
-                    # Running current blob.
-                    RunBlob(BlobInfo, blob)
-
-
-        break
-        
-        
-    
+    # Resolving and running blobs.
+    BlobsInfo, BlobFileNames = ResolveBlobs(version)
+    RunBlobs(BlobsInfo, BlobFileNames)
