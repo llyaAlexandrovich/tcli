ca2fc80349604cb4fb0d2e9d35c93747552fb0d9
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-01-27 18:15:01 +0200

    I'm still in need to understand how does tdata works but i'd rather just make simple configs system for now.

--- src/config/tdata/tdata.cpp
+++ src/config/tdata/tdata.cpp
@@ -7,3 +7,20 @@
 
 
 
+[[nodiscard]] bool IsValidTDF(std::string& const FileContent) noexcept
+{
+    // This might seem to be pretty stupid but here we are.
+    if(FileContent[0] == 'T' && FileContent[1] == 'D' &&\
+        FileContent[2] == 'F' && FileContent[3] == '$')
+    {
+        return true;
+    }
+    return false;
+}
+
+
+
+[[noreturn]]void ReadTDFConfig(std::string_view FileName, std::string& FileContent) noexcept
+{
+    
+}
--- src/config/tdata/tdata.hpp
+++ src/config/tdata/tdata.hpp
@@ -3,6 +3,8 @@
 
 
 #include <iostream>
+#include <cstdint>
+#include <cstddef>
 #include <string>
 #include <string_view>
 
@@ -11,5 +13,49 @@
 #include "spdlog/spdlog.h"
 
 
+/**
+ * Concept
+ * [magic bytes](4)-[version](4)
+ */
 
-[[nodiscard]]bool IsValidTDF(std::string_view FIleName) noexcept;
+
+
+/**
+ * Check whether file is valid TDF format.
+ * 
+ * @author Ilya Alexandrovich
+ * 
+ * @param FileContent  file buffer
+ * 
+ * @return return whether true if file is valid or false
+ * 
+ * @since 1.0.0
+ */
+[[nodiscard]] bool IsValidTDF(std::string& const FileContent) noexcept;
+
+
+
+/**
+ *  Read and examine TDF config.
+ * 
+ * @author Ilya Alexandrovich
+ * 
+ * @param FileName  read-only name of the file
+ * @param FileContent  buffer to read config to
+ * 
+ * @since 1.0.0
+ */
+[[noreturn]] void ReadTDFConfig(std::string_view FileName, std::string& FileContent) noexcept;
+
+
+
+/**
+ * 
+ */
+typedef struct _TDF
+{
+    std::uint32_t version;
+    std::byte PartialMD5;
+    bool IsMD5Correct;
+    std::shared_ptr<std::string> data;
+} TDF;
