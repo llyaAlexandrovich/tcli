434c8a9aab4a5bd534ebbf91032087edf8637adc
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-01-28 13:07:02 +0200

    Upgrade convenient_string class.

--- include/convenient_string.hpp
+++ include/convenient_string.hpp
@@ -21,16 +21,17 @@ public:
      * 
      * @author Ilya Alexandrovich
      * 
-     * @param delimiter  the symbol to use as a delimiter for the string splitting
+     * @param delimiter  the symbol to use as the delimiter for the string splitting
      * 
      * @return list of a resulting tokens
      * 
      * @since 1.0.0
      */
-    [[nodiscard]] std::vector<std::string> extern_split(char const delimiter) noexcept
+    [[nodiscard]] std::vector<std::string> split(char const delimiter) noexcept
     {
-        std::vector<std::string>* output = std::make_shared<std::vector<std::string>>();
-        std::string StrippedString = this->extern_strip();
+        std::vector<std::string> output;
+        std::string StrippedString;
+        StrippedString.assign(this->data());
         std::string word;
 
         for(int counter = 0; counter != this->length(); ++counter)
@@ -51,33 +52,6 @@ public:
 
 
     /**
-     * Split a string using a custom delimiter and rewrite current string content
-     * as a list of tokens using space as the delimiter.
-     * 
-     * @author Ilya Alexandrovich
-     * 
-     * @param delimiter  the symbol to use as a delimiter for the string splitting
-     * 
-     * @return list of a resulting tokens
-     * 
-     * @since 1.0.0
-     */
-    [[noreturn]] void split(std::string const delimiter) noexcept
-    {
-        this->strip();
-
-        for(int counter = 0; counter != this->length(); ++counter)
-        {
-            if(this[counter] == delimiter)
-            {
-                this->erase(counter, 1);
-            }
-        }
-    }
-
-
-
-    /**
      * Exclude all the control symbols such as \n and \r from the string content.
      * Return the resulting string.
      * 
@@ -87,19 +61,19 @@ public:
      * 
      * @since 1.0.0
      */
-    //[[nodiscard]] std::string extern_strip() noexcept
-    //{
-    //    std::string output;
-    //    output.assign(this->data());
-    //    for(int counter = 0; counter != output.length(); ++counter)
-    //    {
-    //        if(output[counter] == '\n' || output[counter] == '\r')
-    //        {
-    //            output.erase(counter, 1);
-    //        }
-    //    }
-    //    return output;
-    //}
+    [[nodiscard]] std::string extern_strip() noexcept
+    {
+        std::string output;
+        output.assign(this->data());
+        for(int counter = 0; counter != output.length(); ++counter)
+        {
+            if(output[counter] == '\n' || output[counter] == '\r')
+            {
+                output.erase(counter, 1);
+            }
+        }
+        return output;
+    }
 
 
 
@@ -134,5 +108,61 @@ public:
     [[noreturn]] void to_lower() noexcept
     {
         std::transform(this->begin(), this->end(), this->begin(), [](unsigned char c){ return std::tolower(c); });
+    }
+
+
+    
+    /**
+     * Convert characters to lower and return resulting string.
+     * 
+     * @author Ilya Alexandrovich
+     * 
+     * @return return string with all characters set as lower
+     * 
+     * @since 1.0.0
+     */
+    [[nodiscard]] std::string extern_to_lower() noexcept
+    {
+        std::string output;
+        output.assign(this->data());
+
+        std::transform(output.begin(), output.end(), output.begin(), [](unsigned char c){ return std::tolower(c); });
+
+        return output;
+    }
+
+
+
+    /**
+     * Convert string characters to upper.
+     * 
+     * @author Ilya Alexandrovich
+     * 
+     * @since 1.0.0
+     */
+    [[noreturn]] void to_upper() noexcept
+    {
+        std::transform(this->begin(), this->end(), this->begin(), [](unsigned char c){ return std::toupper(c); });
+    }
+
+
+
+    /**
+     * Convert characters to upper and return resulting string.
+     * 
+     * @author Ilya Alexandrovich
+     * 
+     * @return return string with all characters set as upper
+     * 
+     * @since 1.0.0
+     */
+    [[nodiscard]] std::string extern_to_upper() noexcept
+    {
+        std::string output;
+        output.assign(this->data());
+
+        std::transform(output.begin(), output.end(), output.begin(), [](unsigned char c){ return std::toupper(c); });
+
+        return output;
     }
 };
