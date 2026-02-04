31df1d79fdd354377cefe965e534769c9491e283
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-02-04 14:57:39 +0200

    Update main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -8,11 +8,13 @@
 #include <format>
 #include <thread>
 #include <map>
+#include <memory>
 
 
 
 #include "helper/helper.hpp"
 #include "localization/localization.hpp"
+#include "render/text/text_render.hpp"
 
 
 #include "spdlog/spdlog.h"
@@ -279,7 +281,7 @@ private:
             return;
         }
 
-        if(response.request_id == 0)
+        if(response.request_id == 0) // Server response.
         {
             return ProcessUpdate(std::move(response.object));
         }
@@ -297,37 +299,47 @@ private:
     [[noreturn]] void ProcessUpdate(TdObject update) // td_api::object_ptr<td_api::Object>
     {
         td_api::downcast_call(
-        *update, overloaded(
-                    [this](td_api::updateAuthorizationState &update_authorization_state) {
+        *update, 
+                overloaded(
+                    [this](td_api::updateAuthorizationState &update_authorization_state) 
+                    {
                         AuthtorizationState = std::move(update_authorization_state.authorization_state_);
                         OnAuthStateUpdate();
                     },
-                    [this](td_api::updateNewChat &update_new_chat) {
+                    [this](td_api::updateNewChat &update_new_chat) 
+                    {
                         ChatTitle[update_new_chat.chat_->id_] = update_new_chat.chat_->title_;
                     },
-                    [this](td_api::updateChatTitle &update_chat_title) {
+                    [this](td_api::updateChatTitle &update_chat_title) 
+                    {
                         ChatTitle[update_chat_title.chat_id_] = update_chat_title.title_;
                     },
-                    [this](td_api::updateUser &update_user) {
+                    [this](td_api::updateUser &update_user) 
+                    {
                         auto user_id = update_user.user_->id_;
                         users[user_id] = std::move(update_user.user_);
                     },
-                    [this](td_api::updateNewMessage &update_new_message) {
+                    [this](td_api::updateNewMessage &update_new_message)
+                    {
                         auto chat_id = update_new_message.message_->chat_id_;
                         std::string sender_name;
-                        td_api::downcast_call(*update_new_message.message_->sender_id_, overloaded(
-                                                [this, &sender_name](td_api::messageSenderUser &user) {
+                        td_api::downcast_call(*update_new_message.message_->sender_id_, 
+                                            overloaded(
+                                                [this, &sender_name](td_api::messageSenderUser &user) 
+                                                {
                                                     sender_name = GetUserName(user.user_id_);
                                                 },
-                                                [this, &sender_name](td_api::messageSenderChat &chat) {
+                                                [this, &sender_name](td_api::messageSenderChat &chat) 
+                                                {
                                                     sender_name = GetChatTitle(chat.chat_id_);
                                                 }));
-                    std::string text;
-                    if (update_new_message.message_->content_->get_id() == td_api::messageText::ID) {
-                        text = static_cast<td_api::messageText &>(*update_new_message.message_->content_).text_->text_;
-                    }
-                    std::cout << "Receive message: [chat_id:" << chat_id << "] [from:" << sender_name << "] ["
-                                << text << "]" << std::endl;
+                        std::string text;
+                        if (update_new_message.message_->content_->get_id() == td_api::messageText::ID) 
+                        {
+                            text = static_cast<td_api::messageText &>(*update_new_message.message_->content_).text_->text_;
+                        }
+                        std::cout << "Receive message: [chat_id:" << chat_id << "] [from:" << sender_name << "] ["
+                                    << text << "]" << std::endl;
                     },
                     [](auto &update) {}));
     }
