47040f7733f7f2cafdf089facc4948ed5526c5cf
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-02-04 14:55:51 +0200

    Update localization.hpp .
    	modified:   src/localization/localization.hpp

--- src/localization/localization.hpp
+++ src/localization/localization.hpp
@@ -10,12 +10,8 @@
 
 
 #if defined _WIN32 || defined _WIN64
-
-
 #include <windows.h>
 #include <codecvt>
-
-
 
 /**
  * Get system locale.
@@ -45,12 +41,35 @@
 
 
 
+/**
+ * Get system locale as is using IETF standard.
+ * 
+ * @author Ilya Alexandrovich
+ * 
+ * @return whether system locale or std::string.empty()
+ * 
+ * @since 1.0.0
+ */
+[[nodiscard]] std::string GetSystemLocaleWide() noexcept
+{
+    std::wstring WinLocaleName;
+
+    if(GetUserDefaultLocaleName(WinLocaleName.data(), LOCALE_NAME_MAX_LENGTH) == 0)
+    {
+        return std::string();
+    }
+
+    // !NOTE: This function is deprecated since cxx17.
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    std::string output = converter.to_bytes(WinLocaleName);
+
+    return output;
+}
+
+
+
 #else // Linux and others.
-
-
 #include <clocale>
-
-
 
 /**
  * Get system locale.
@@ -75,8 +94,24 @@
 
 
 
+/**
+ * Get system locale as is using IETF standard.
+ * 
+ * @author Ilya Alexandrovich
+ * 
+ * @return whether system locale or std::string.empty()
+ * 
+ * @since 1.0.0
+ */
+[[nodiscard]] std::string GetSystemLocaleWide() noexcept
+{
+    const char* LinuxLocaleName = std::setlocale(LC_ALL, "");
+    if(LinuxLocaleName != nullptr)
+    {
+        std::string output(LinuxLocaleName);
+        return output;
+    }
+    return std::string();
+}
+
 #endif // Linux and others.
-
-
-
-
