7205c5dd088bfe70990a213f7751cbdc6ae437dc
Ilya Alexandrovich <marmanplay9@gmail.com>
2026-02-09 17:05:36 +0200

    Add robust way to include requried info from tdlib

---
+++ src/tdhelper/tdtypes.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+
+
+#include "td/telegram/Client.h"
+#include "td/telegram/td_api.h"
+#include "td/telegram/td_api.hpp"
+
+
+// Type Dispatching.
+namespace detail {
+template <class... Fs>
+struct overload;
+
+
+template <class F>
+struct overload<F> : public F {
+    explicit overload(F f) : F(f) {
+}
+};
+
+
+template <class F, class... Fs>
+struct overload<F, Fs...> : public overload<F>, public overload<Fs...> {
+    overload(F f, Fs... fs) : overload<F>(f), overload<Fs...>(fs...) {}
+    using overload<F>::operator();
+    using overload<Fs...>::operator();
+};
+
+}  // namespace detail
+
+
+template <class... F>
+auto overloaded(F... f) {
+    return detail::overload<F...>(f...);
+}
+// ...Type Dispatching.
+
+
+// Helpers.
+namespace td_api = td::td_api;
+
+using TdObject = td_api::object_ptr<td_api::Object>;
+
+
+// Prefixes.
+// Prefix for chat name.
+#define GROUP_CHAT_PREFIX L"✤"
+
+
+
+// Consts.
+// Max length of the full name to show as a preview.
+#define MAX_FULL_NAME_LENGTH 10
+
+
